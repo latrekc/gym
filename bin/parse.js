54030cbb447dbb05e9fd826d45de0fa38e54c59c
Stanislav Tugovikov <s.tugovikov@corp.mail.ru>
2017-06-11 00:09:31 +0300

    inital parser

--- bin/parse.js
+++ bin/parse.js
@@ -1,4 +1,6 @@
 #!/usr/bin/env node
+require('colors');
+const fs = require('fs');
 
 class Workout {
 	constructor (source) {
@@ -33,12 +35,84 @@ class Workout {
 	} 
 }
 
+let parser = require("pegjs").buildParser(fs.readFileSync('parser.pegjs', 'utf-8'));
+let source = fs.readFileSync('data.txt', 'UTF-8') + "\n";
 
+function hr() {
+	var space = [];
+	for(var i=0; i<process.stdout.columns; i++) {
+		space.push(' ');
+	}
+	console.log(space.join('').inverse)
+}
 
-require('fs').readFileSync('data.txt', 'UTF-8').split(/\n{2,}/).map((source) => {
-	console.log(source, "\n======================");
-	let item = new Workout(source);
+function showSource(location) {
+	hr();
 
-//	console.log(item);
-	console.log();
-});
+	console.log(source.split(/\n/).map((line, i) => {
+		var ln = (i+1) + '';
+
+		if(location) {
+			if(location.start.line === (i + 1)) {
+				var first = line.substr(0, location.start.column - 1);
+
+				if(location.start.line == location.end.line) {
+					line = first + line.substr(location.start.column - 1, location.end.column - location.start.column).inverse.red + line.substr(location.end.column - 1);
+
+				} else {
+					line = first + line.substr(location.start.column - 1).inverse.red;
+				}
+
+				ln = ln.inverse.red;
+
+			} else if (location.end.line === (i + 1)) {
+				var first = line.substr(0, location.end.column - 1);
+				line = first + line.substr(location.end.column - 1).inverse.red;
+				ln = ln.inverse.red;
+			
+			} else if (location.start.line < (i + 1) && (i + 1) < location.end.line ){
+				line = line.inverse.red;
+				ln = ln.inverse.red;
+			}
+		}
+
+		return ln + ' ' + line
+
+	}).join("\n"));
+	console.log()
+}
+
+let result;
+
+try {
+	result = parser.parse(source, {
+		debug: true,
+	})
+
+	if(result) {
+		showSource();
+
+		result.forEach((line, number) => {
+			if(!line) {
+				console.log('NOT LINE', line)
+				process.exit()
+			} else {
+				console.log(number.toString().blue, JSON.stringify((line || ''), null, "\t").yellow)
+				console.log();
+			}
+		})
+	}
+
+} catch(e) {
+	showSource(e.location);
+
+	console.error(e.message.red)
+	if(e.location) {
+		console.error(JSON.stringify(e.location, null, "\t").red)
+	}
+	process.exit()
+}
+
+console.log()
+console.log()
+console.log()
---
+++ parser.pegjs
@@ -0,0 +1,90 @@
+{
+	function _flatten(arr) {
+		if(!Array.isArray(arr)) {
+			return arr;
+		}
+
+		return arr.filter(i => i !== null && i !== "\n").reduce((flat, toFlatten) => {
+			return flat.concat(Array.isArray(toFlatten) ? _flatten(toFlatten) : toFlatten);
+		}, []);
+	}
+
+	function flatten(arr) {
+		var res = _flatten(arr);
+		return res && res.length == 1 ? res[0] : res;
+	}
+
+	function hash(name, value) {
+		var result = {}
+		result[name] = value;
+		return result;
+	}
+
+	function node(result) {
+		return [{
+			text: text(),
+			result:flatten(result),
+			ln: location().start.line
+		}]
+	}
+}
+
+start =  result:TRAINING+ {
+	return result;
+}
+
+TRAINING = caption:(CAPTION / REVERSE_CAPTION) NL sets:SET+ (SPACE / NL)* {
+	return {
+		caption,
+		sets,
+		source:text()
+	}
+}
+
+SET = number:([0-9]) ")" result:EXERCISE tail:(NL? "," text2:EXERCISE)* NL {
+	tail = flatten(tail);
+
+	if(tail.length) {
+		result += tail.join('');
+	}
+
+	return { number, result };
+}
+
+EXERCISE = !([0-9] ")") TEXT {
+	return text();
+}
+
+EMPTY = ""
+
+LB = "("
+
+RB = ")"
+
+NL = [\n]+
+
+SPACE = [ \t]+
+
+WEEKDAY = day:("Понедельник" / "Вторник" /  "Среда" / "Четверг" / "Пятница" / "Суббота" / "Воскресенье" / "Пон") {
+	if(day === "Пон") {
+		return "Понедельник";
+	}
+
+	return day;
+}
+
+TEXT = [A-zА-яё0-9, ()\+:\-]+ {
+	return text();
+}
+
+DECRIPTION = !([0-9] ")") TEXT {
+	return text();
+}
+
+CAPTION = weekday:WEEKDAY ":"? caption:(NL? text:DECRIPTION (NL DECRIPTION)*)? {
+	return { weekday, descr: flatten(caption) };
+}
+
+REVERSE_CAPTION = caption:(text:DECRIPTION) NL weekday:WEEKDAY ":" {
+	return { weekday, descr: flatten(caption) };
+}
